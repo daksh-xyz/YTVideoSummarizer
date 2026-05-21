bca2830f748b779f16e073a0140cd32cd4ceda6f
Daksh Lal <111529348+daksh-xyz@users.noreply.github.com>
2026-05-21 10:48:15 +0530

    Fix method name to get_transcript in app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ class YouTubeSummarizer:
         """Get transcript from YouTube video in preferred language"""
         try:
             video_id = self.extract_video_id(youtube_url)
-            transcript_list = YouTubeTranscriptApi.get_transcripts(video_id)
+            transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
             
             # Get available transcripts info for user feedback
             available_languages = []
@@ -178,7 +178,7 @@ class YouTubeSummarizer:
         """Get list of available transcripts for a video"""
         try:
             video_id = self.extract_video_id(youtube_url)
-            transcript_list = YouTubeTranscriptApi.get_transcripts(video_id)
+            transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
             
             transcripts_info = []
             for transcript in transcript_list:
